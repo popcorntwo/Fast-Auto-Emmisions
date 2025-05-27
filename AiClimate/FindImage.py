23c74c48604d5684f0b7911d7395c1dcd5897761
popcorntwo <147192940+popcorntwo@users.noreply.github.com>
2025-05-27 10:23:16 -0700

    update 1.1

--- AiClimate/FindImage.py
+++ AiClimate/FindImage.py
@@ -4,31 +4,14 @@ import os
 
 
 path_to_folder = os.path.join(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))),"Basket")
-#entries = os.listdir(path_to_folder)
-#path_to_image = os.path.join(path_to_folder, entries[0])
-#model_path = (r"C:\Users\popko\OneDrive\Documents\GitHub\AiClimate\AiClimate\keras_model.h5")
 model_path = os.path.join(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))),"keras_model.h5")
-#labels_file_path = (r"C:\Users\popko\OneDrive\Documents\GitHub\AiClimate\AiClimate\labels.txt")
 labels_file_path = os.path.join(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))), "labels.txt")
-
 
 def empty_basket():
    os.remove(os.path.join(path_to_folder, os.listdir(path_to_folder)[0]))
 
-# Initialise the TeachableMachine
-#model = TeachableMachine(model_path=model_path, labels_file_path=labels_file_path)
-# Classify the image
-#result = model.classify_image(path_to_image)
-# Print results
-#print("Shape Label:", result['class_index'])
-#print("Shape Name:", result['class_name'])
-path_to_image = os.path.join(path_to_folder, path_to_folder[0])
-print(path_to_image)
-
-def FindImage():
+def find_image():
     model = TeachableMachine(model_path=model_path, labels_file_path=labels_file_path)
-    print(path_to_folder[0])
-    path_to_image = os.path.join(path_to_folder, os.listdir(path_to_folder)[0])
-    result = model.classify_image(path_to_image)
+    result = model.classify_image(os.path.join(path_to_folder,os.listdir(path_to_folder)[0]))
     empty_basket()
     return result['class_name']
--- AiClimate/FlaskServer.py
+++ AiClimate/FlaskServer.py
@@ -1,188 +1,99 @@
-import json
 import os
-import FindImage
+import FindImage as fi
 import CO2Calcs as calcs
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-#UPLOAD_FOLDER = r"C:\Users\popko\OneDrive\Documents\GitHub\AiClimate\AiClimate\Basket"
+from dataclasses import dataclass
+
 UPLOAD_FOLDER = os.path.join(os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__))), "Basket")
 ALLOWED_EXTENSION = set(['jpeg','jpg'])
+RECCOMENDED_CO2 = 23000000
 
+#better stucture support have everything at base be set to None
+@dataclass
+class Validation_Result:
+    error: tuple = None
+    milage: float = -1.0
+    car_type: str = ""
+    co2_per_kilometer: int = 0
+ 
+#functions
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.',1)[1].lower() in ALLOWED_EXTENSION
 
 def empty_basket():
    os.remove(os.path.join(UPLOAD_FOLDER, os.listdir(UPLOAD_FOLDER)[0]))
 
-app = Flask(__name__)
-
-@app.route("/media/upload/co2perkilometer/report", methods = ['POST'])
-def co2_per_kilometer_report():
-    
-
-    car_type = ""
-
-    if(False):
-        return jsonify({'error': ' other media being processed'})
-    
-    if 'file' not in request.files:
+def validate_request(require_milage = False):
+    returning_validation_result = Validation_Result()
+    image_handle_error = auth_and_upload_image(request)
+    print (image_handle_error)
+    if image_handle_error is None:
+        returning_validation_result.car_type = fi.find_image()[2:].replace(" ","") 
+        returning_validation_result.co2_per_kilometer = calcs.calculte_co2_per_kilometer(returning_validation_result.car_type)
+    else:
+        return Validation_Result(error=image_handle_error)
+    if require_milage:
+        milage_handle_error = auth_milage(request)
+        if milage_handle_error is None:
+            return Validation_Result(error=milage_handle_error)
+        else:
+            returning_validation_result.milage=milage_handle_error
+    return returning_validation_result
+        
+#returns eiter tuple if there is a error or null
+def auth_and_upload_image(arg):
+    if 'file' not in arg.files:
         return jsonify({'error': 'media not provided'}),400
+    file = arg.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'no file selected'}),400
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(UPLOAD_FOLDER, str(filename)))
+    else:
+         return jsonify({'error': 'file not proper'}),500
+    return None
 
-    if request.is_json:
+#returns tuple if hits error or returns the milage as a float
+def auth_milage(request):
+    milage = -1
+    if(request.is_json):
         data = request.get_json(silent=True) or {}
         milage = data.get('milage')
     else:
         milage = request.form.get('milage')
-
     if milage is None:
         return jsonify({'error': 'no milage detected'}),400
-
-  
-    file = request.files['file']
-    print(file)
-    
-    if file.filename == '':
-        return jsonify({'error': 'no file selected'}),400
-    
-    
-    milage = request.form['milage']
-
-    if(not milage.isdigit()):
+    if not milage.isdigit():
         return jsonify({'error': 'milage contains non-numbers'}),400
-    
     milage = float(milage)
     
-    print(milage)
-  
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        print(UPLOAD_FOLDER)
-        file.save(os.path.join(UPLOAD_FOLDER, str(filename)))
-        print("media uploaded successfully. processing")
-        
-        car_type = FindImage.FindImage()[2:].replace(" ","")
-      
-        
-        if(car_type == ""):
-            return jsonify({'msg': 'error processing image'}),500
-    else:
-         return jsonify({'msg': 'file not proper'}),500
-    
-    print(car_type)
-    print(milage)
-    print(calcs.calculte_co2_per_kilometer(car_type))
-    return jsonify({'msg':  calcs.report(milage,calcs.calculte_co2_per_kilometer(car_type))})
+    return milage
 
+#main app
+app = Flask(__name__)
 
-
-
-
-
-
-
-
+#Routes
+@app.route("/media/upload/co2perkilometer/report", methods = ['POST'])
+def co2_per_kilometer_report():
+    msg = validate_request(require_milage=True)
+    if msg.error is None: return jsonify({'msg':  calcs.report(msg.milage,calcs.calculte_co2_per_kilometer(msg.car_type))})
+    return msg.error
 
 @app.route("/media/upload/co2perkilometer/percentthisyear", methods = ['POST'])
 def co2_per_kilometer_percentthisyear():
-    
-
-    car_type = ""
-
-    if(False):
-        return jsonify({'error': ' other media being processed'})
-    
-    if 'file' not in request.files:
-        return jsonify({'error': 'media not provided'}),400
-
-    if request.is_json:
-        data = request.get_json(silent=True) or {}
-        milage = data.get('milage')
-    else:
-        milage = request.form.get('milage')
-
-    if milage is None:
-        return jsonify({'error': 'no milage detected'}),400
-
-  
-    file = request.files['file']
-    print(file)
-    
-    if file.filename == '':
-        return jsonify({'error': 'no file selected'}),400
-    
-    
-    milage = request.form['milage']
-
-    if(not milage.isdigit()):
-        return jsonify({'error': 'milage contains non-numbers'}),400
-    
-    milage = float(milage)
-    
-    print(milage)
-  
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        print(UPLOAD_FOLDER)
-        file.save(os.path.join(UPLOAD_FOLDER, str(filename)))
-        print("media uploaded successfully. processing")
-        
-        car_type = FindImage.FindImage()[2:].replace(" ","")
-      
-        
-        if(car_type == ""):
-            return jsonify({'msg': 'error processing image'}),500
-    else:
-         return jsonify({'msg': 'file not proper'}),500
-    percent = ((calcs.calculte_co2_per_kilometer(car_type) * milage) / 23000000) * 100
-    return jsonify({'msg':  str(int(percent))})
-
-
-
-
-
-
-
-
+    msg = validate_request(require_milage=True) 
+    if msg.error is None:
+        percent = (calcs.calculte_co2_per_kilometer(msg.car_type) * msg.milage) / RECCOMENDED_CO2 * 100
+        return jsonify({'msg':  str(int(percent))})
+    return msg.error
 
 @app.route("/media/upload/co2perkilometer", methods = ['POST'])
 def co2_per_kilometer():
-
-    car_type = ""
-
-    if(False):
-        return jsonify({'error': ' other media being processed'})
-    
-    if 'file' not in request.files:
-        return jsonify({'error': 'media not provided'}),400
-    
-    file = request.files['file']
-    print(file)
-    
-    if file.filename == '':
-        return jsonify({'error': 'no file selected'}),400
-    
-  
-    if file and allowed_file(file.filename):
-        filename = secure_filename(file.filename)
-        print(UPLOAD_FOLDER)
-        file.save(os.path.join(UPLOAD_FOLDER, str(filename)))
-        print("media uploaded successfully. processing")
-        
-        car_type = FindImage.FindImage()[2:].replace(" ","")
-
-        
-        if(car_type == ""):
-            return jsonify({'msg': 'error processing image'}),500
-    else:
-         return jsonify({'msg': 'file not proper'}),500
-
-    print(calcs.calculte_co2_per_kilometer(car_type))
-    return jsonify({'msg':  calcs.calculte_co2_per_kilometer(car_type)})
-    
+    msg: validate_request = validate_request(require_milage=True) 
+    if msg.error is None: return jsonify({'msg':  msg.co2_per_kilometer})
+    return msg.error
 
 if(__name__ == '__main__'): 
     app.run(debug= True, port=5000)
-
-def media_finished_being_used():
-    using_media = False
-
